60978c7e899fb38962f588163029a88dffb95e43
belpavz <i@belpav.ru>
2025-04-13 00:47:35 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -17,6 +17,13 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         f'postgresql://ycbotuser:{DATABASE_PASSWORD}@localhost/ycbase?sslmode=require&keepalives=1&keepalives_idle=60&keepalives_interval=10&keepalives_count=5'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    YCLIENTS_CLIENT_ID = os.environ.get('YCLIENTS_CLIENT_ID')
-    YCLIENTS_CLIENT_SECRET = os.environ.get('YCLIENTS_CLIENT_SECRET')
-    OAUTH_REDIRECT_URI = os.environ.get('OAUTH_REDIRECT_URI')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.timeweb.ru')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in [
+        'true', '1', 't']
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL', 'false').lower() in [
+        'true', '1', 't']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.environ.get(
+        'MAIL_DEFAULT_SENDER') or os.environ.get('MAIL_USERNAME')
